18c67abb6b6c87effe174251e708750f2cf97240
Trolled00Gamer <aryu100@gmail.com>
2021-06-30 17:57:39 +0530

    fixed the auth system (email and google-sign-in) and it works now

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,33 +1,78 @@
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uberfreight/user.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
-Future<String> signInWithGoogle() async {
-  try {
-    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-    final GoogleSignInAuthentication googleSignInAuthentication =
-        await googleSignInAccount.authentication;
+String name;
+String email;
+String uid;
+String phoneNumber;
+String photoUrl;
 
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken,
-    );
+class AuthService {
+  Future signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
 
-    final AuthResult authResult = await _auth.signInWithCredential(credential);
-    final FirebaseUser user = authResult.user;
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
 
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
+      final AuthResult authResult =
+          await _auth.signInWithCredential(credential);
+      final FirebaseUser user = authResult.user;
 
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
-    if (user == null) print('not signed in');
-    else print('signed in' + user.uid);
-    return 'signInWithGoogle succeeded: $user';
-  } catch (error) {
-    print(error);
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
+
+      final FirebaseUser currentUser = await _auth.currentUser();
+      assert(user.uid == currentUser.uid);
+      if (user == null)
+        print('not signed in');
+      else
+        print('signed in' +
+            user.uid +
+            user.displayName +
+            user.email +
+            user.photoUrl);
+
+      uid = user.uid;
+      name = user.displayName;
+      email = user.email;
+      phoneNumber = user.phoneNumber;
+      photoUrl = user.photoUrl;
+
+      return _firebaseUser(user);
+
+      // return User(user.uid, user.displayName, user.email, user.phoneNumber,
+      //         user.photoUrl)
+      //     .toString();
+    } catch (error) {
+      print(error);
+    }
+    return null;
   }
-  return null;
+
+  Stream<User> get user {
+    print('streamer running');
+    return _auth.onAuthStateChanged.map(_firebaseUser);
+  }
+
+  User _firebaseUser(FirebaseUser user) {
+    user != null ? print('user is not null') : print('user is null');
+    return user != null
+        ? User(
+            uid: user.uid,
+            phoneNumber: user.phoneNumber,
+            email: user.email,
+            name: user.displayName,
+            photoUrl: user.photoUrl)
+        : null;
+  }
 }
